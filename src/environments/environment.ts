b641f535213daa1b59b1bac427ff953d1452be02
elamrisaifeddinepro <elamrisaifeddinepro@gmail.com>
2026-04-27 19:31:57 -0400

    docs: add project screenshots and update environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
   authApiUrl: 'https://locationhabitat-auth-service.onrender.com/api/auth',
   businessApiUrl: 'https://locationhabitat-business-service.onrender.com/api',
 
-  supabaseUrl: 'https://tozjblocziztrcdficfr.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvempibG9jeml6dHJjZGZpY2ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4Mzc3ODMsImV4cCI6MjA4NjQxMzc4M30.R4yRz33yT6SgYOQViENhQ9Mpjv8dLjLzYjEeDpoxGnQ'
+  supabaseUrl: '',
+  supabaseAnonKey: ''
 };
